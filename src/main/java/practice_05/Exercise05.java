356f4f1cc1bb7ed108b119843b93b1cfe996bf3c
farukkocabas <faruk.kocabas280@gmail.com>
2024-05-26 23:37:34 +0300

    yeni çalışma

---
+++ src/main/java/practice_05/Exercise05.java
@@ -0,0 +1,19 @@
+package practice_05;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+
+                //  çarpım tablosunu yaz 10x10 'a kadar olan çarpım tablosu.
+
+        for (int i = 1; i <11 ; i++) {
+            for (int k = 1; k <11 ; k++) {
+
+                System.out.print(i+"x"+k+"="+(i*k)+"\td");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
