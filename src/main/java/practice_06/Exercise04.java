73f04ee52a1a4a4646825bf5529776f6af692051
farukkocabas <faruk.kocabas280@gmail.com>
2024-05-27 21:56:28 +0300

    practice_06 exercise04 mdArray;practice_07 exercise01 method oluşturma

---
+++ src/main/java/practice_06/Exercise04.java
@@ -0,0 +1,48 @@
+package practice_06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+
+    /*
+         -->Iki boyutlu bir Array’i tek boyutlu bir Array’e ceviriniz.
+           int[][] numbers = {{5, 4}, {2, 3, 2}}; ==> { 5,4,2,3,2 }
+           sonrasında da tek boyutlu array in elemanların toplamını yazdır.
+   */
+
+
+        int[][] numbers = {{15,27}, {22, 73, 12}};
+        System.out.println(Arrays.deepToString(numbers));//[[15, 27], [22, 73, 12]]
+        ArrayList<Integer> num=new ArrayList<>();
+
+        for (int[] w : numbers){
+            for (int k : w){
+                num.add(k);
+            }
+
+        }
+        System.out.println(num);//[15, 27, 22, 73, 12]
+
+        //-->int turunde bir Multidimensional Array’deki en kucuk ve en buyuk elemanin toplamini bulunuz.
+        System.out.println(Arrays.deepToString(numbers)); //[[15, 27], [22, 73, 12]]
+        int smallest=numbers[0][0];
+        int biggest=numbers[0][0];
+
+        ArrayList<Integer> num1 =new ArrayList<>();
+        for (int[] w : numbers){
+            for (int k :w){
+                smallest=Math.min(smallest,k);  // 12
+                biggest=Math.max(biggest,k);    // 73
+            }
+        }
+        System.out.println("Multidimensional Array deki en küçük eleman: "+smallest+
+                            "\nMultidimensional Array deki en büyük eleman: "+biggest);
+
+
+
+
+
+    }
+}
