0ceeb77880e7fe6ed140e188b9fa31a7da31b843
farukkocabas <faruk.kocabas280@gmail.com>
2024-05-27 16:08:32 +0300

    güzel bir döngü ve if çalışması oldu

--- src/main/java/practice_05/Exercise05.java
+++ src/main/java/practice_05/Exercise05.java
@@ -1,5 +1,7 @@
 package practice_05;
 
+import java.util.Scanner;
+
 public class Exercise05 {
     public static void main(String[] args) {
 
@@ -13,6 +15,31 @@ public class Exercise05 {
             }
             System.out.println();
         }
+
+  /*       Ornek 1:
+           Belirli bir tamsayının palindrome olup olmadığını kontrol eden kodu yazın.
+         Palindrome: 121 <==> 121    123321 <==> 123321
+*/
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("Lütfen bir tam sayı giriniz.");
+        int num = input.nextInt();
+        String num1=String.valueOf(num);
+        System.out.println(num1);
+
+        String reverseNum ="";
+        for (int i = num1.length()-1; i > -1; i--) {
+            reverseNum+=num1.charAt(i);
+
+        }
+        System.out.println(reverseNum);
+        if (reverseNum.equals(num1)){
+            System.out.println("Girdiğiniz sayı palindrome bir sayıdır.");
+        }else {
+            System.out.println("Girdiğiniz sayı palindrome bir sayı değil.");
+        }
+
+
 
 
     }
